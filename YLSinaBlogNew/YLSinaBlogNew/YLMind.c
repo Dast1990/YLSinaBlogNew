3762c22bae858a530d0e355b115d6a011b87a9aa
Dast1 <myl90@126.com>
2015-12-05 00:46:57 +0800

    AFN更改为2.6.3了，3beta不好用

--- YLSinaBlogNew/YLSinaBlogNew/YLMind.c
+++ YLSinaBlogNew/YLSinaBlogNew/YLMind.c
@@ -5,9 +5,7 @@
 //  Created by LongMa on 15/11/29.
 //  Copyright © 2015年 LongMa. All rights reserved.
 //
-/* 
-aece6e0 HEAD@{8}: clone: from https://github.com/Dast1990/sinaBlogOC.git
- 
+/*
 V1.在tabBarC基础上显示了4个子控制器的界面。设置了pch文件，测试用的xib。设置了子控制器的leftBarButtonItem属性，待完善自定义导航控制器。
 V2.写UIView分类，方直接.size,.x 等快速代码
 V3. 统一所有导航控制器左上角和右上角的内容。
@@ -23,7 +21,9 @@ V8.在消息界面为导航栏右item设置文字，设置为不可用，并使�
     切换界面回来（viewWillAppear重新设置了颜色为亮灰色）又变回了全局橘色，why?
      A:因为appearance设置会与tinitColor会冲突。 不设置tinitColor而在全局对象设置不可用状态颜色就好了！
 V9.设置发现界面导航栏的搜索框
-
-
+V10.设置首页下拉菜单
+V11.添加发布微博加号按钮和点击事件
+V12.
+ 
 
 */
--- YLSinaBlogNew/YLSinaBlogNew/classes/OAuth/controller/YLOAuthViewController.m
+++ YLSinaBlogNew/YLSinaBlogNew/classes/OAuth/controller/YLOAuthViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "YLOAuthViewController.h"
-//#import <AFNetworking.h>
+#import <AFNetworking.h>
 
 @interface YLOAuthViewController ()<UIWebViewDelegate>
 @property (nonatomic, weak) UIWebView *webView;
@@ -21,7 +21,7 @@
     UIWebView *webView = [[UIWebView alloc] init];
     
     webView.frame = self.view.bounds;
-//只有拖动到头时，才可以看到背景色
+    //只有拖动到头时，才可以看到背景色
     webView.backgroundColor = [UIColor orangeColor];
     webView.delegate = self;
     [self.view addSubview:webView];
@@ -32,8 +32,6 @@
 
 
 - (void)webViewOperation{
-//    https://api.weibo.com/oauth2/authorize?client_id=1773885185&redirect_uri=http://www.baidu.com/
-//    http://www.baidu.com/
     NSURL *url = [NSURL URLWithString:@"https://api.weibo.com/oauth2/authorize?client_id=2607741795&redirect_uri=http://www.baidu.com/"];
     
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
@@ -42,10 +40,60 @@
 }
 
 #pragma mark -  代理方法
+#pragma mark -  截取网络参数
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
     YLLOG(@"%s", __func__);
     YLLOG(@"%@", request);
+    
+#warning 注意：要熟练
+    //    1.获得url字符串
+    NSString *urlString = request.URL.absoluteString;
+    //    2.判断是否为回调地址（看是否包含 code=，后面跟的就是code数字）
+    NSRange range = [urlString rangeOfString:@"code="];
+    
+    if (range.length) {//是回调地址，code有值
+        //        截取code= 后面参数值
+        NSUInteger fromIndex = range.location + range.length;
+        NSString *code = [urlString substringFromIndex:fromIndex];
+        
+        //        用code换取accessToken
+        [self getTokenByCode:code];
+        return NO;
+    }
     return YES;
+}
+
+- (void)getTokenByCode:(NSString *)code{
+    YLLOG(@"%s", __func__);
+    
+    /*
+     请求参数
+     必选	类型及范围	说明
+     client_id	true	string	申请应用时分配的AppKey。
+     client_secret	true	string	申请应用时分配的AppSecret。
+     grant_type	true	string	请求的类型，填写authorization_code
+     code	true	string	调用authorize获得的code值。
+     redirect_uri	true	string	回调地址，需需与注册应用里的回调地址一致。
+     */
+    
+    //    是字典，不是数组！
+    NSMutableDictionary *dicM = [NSMutableDictionary dictionary];;
+    dicM[@"client_id"] = @"2607741795";
+    dicM[@"client_secret"] = @"3f6587eeeda669d2b1375c8285ecb75d";
+    dicM[@"grant_type"] = @"authorization_code";
+    dicM[@"code"] = code;
+    dicM[@"redirect_uri"] = @"http://www.baidu.com/";
+    
+    //  AFN请求token,下面用 AFHTTPRequestOperationManager创建管理器，实现对应POST方法，修改底层接收格式。也行！
+    AFHTTPSessionManager *sessionManager = [AFHTTPSessionManager manager];
+    //    sessionManager.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    [sessionManager POST:@"https://api.weibo.com/oauth2/access_token" parameters:dicM success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        YLLOG(@"请求成功：%@", responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        YLLOG(@"请求失败：%@", error);
+    }];
+    
 }
 
 - (void)webViewDidStartLoad:(UIWebView *)webView{
@@ -57,9 +105,11 @@
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
-    YLLOG(@"%s", __func__); 
+    YLLOG(@"%s", __func__);
 }
 
-
+- (BOOL)prefersStatusBarHidden{
+    return YES;
+}
 
 @end
